36ff3d4d844ce248d8ddb45b03ccfc51c8d81aa8
Евгений <borovoy@MacBook-Pro-Evgenij.local>
2024-01-29 22:04:47 +0400

    to BCM and admin

--- vacancies/models.py
+++ vacancies/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 
 class Vacancy(models.Model):
-    text = models.CharField(max_length=2000)
+    STATUS = [
+        ("draft", "Черновик"),
+        ("open", "Открыта"),
+        ("closed", "Закрыта")
+    ]
+    slug = models.SlugField(max_length=60)
+    text = models.CharField(max_length=1500)
+    status = models.CharField(max_length=6, choices=STATUS, default="draft")
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.slug
--- vacancies/views.py
+++ vacancies/views.py
@@ -2,7 +2,10 @@ import json
 
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
 
 from vacancies.models import Vacancy
 
@@ -11,40 +14,47 @@ def hello(request):
     return HttpResponse("Hello world")
 
 
-@csrf_exempt
-def index(request):
-    if request.method == "GET":
-        vacancies = Vacancy.objects.all()
-        search_text = request.GET.get("text", None)
-        if search_text:
-            vacancies = vacancies.filter(text=search_text)
+@method_decorator(csrf_exempt, name="dispatch")
+class VacancyView(View):
 
-        response = []
-        for vacancy in vacancies:
-            response.append({
-                "id": vacancy.id,
-                "text": vacancy.text,
-            })
-        return JsonResponse(response, safe=False)
-    elif request.method == "POST":
+    def get(self, request):
+
+        if request.method == "GET":
+            vacancies = Vacancy.objects.all()
+            search_text = request.GET.get("text", None)
+            if search_text:
+                vacancies = vacancies.filter(text=search_text)
+
+            response = []
+            for vacancy in vacancies:
+                response.append({
+                    "id": vacancy.id,
+                    "text": vacancy.text,
+                    "slug": vacancy.slug,
+                })
+            return JsonResponse(response, safe=False)
+
+    def post(self, request):
         vacancy_data = json.loads(request.body)
         vacancy = Vacancy()
         vacancy.text = vacancy_data["text"]
+        vacancy.slug = vacancy_data["slug"]
         vacancy.save()
         return JsonResponse({
             "id": vacancy.id,
             "text": vacancy.text,
+            "slug": vacancy.slug
         })
 
 
-def get(request, vacancy_id):
-    if request.method == "GET":
-        try:
-            vacancy = Vacancy.objects.get(pk=vacancy_id)
-        except Vacancy.DoesNotExist:
-            return JsonResponse({"error: Not found"}, status=404)
+class VacancyDetailView(DetailView):
+    model = Vacancy
+
+    def get(self, *args, **kwargs):
+        vacancy = self.get_object()
         response = ({
             "id": vacancy.id,
             "text": vacancy.text,
+            "slug": vacancy.slug
         })
         return JsonResponse(response, safe=False)
